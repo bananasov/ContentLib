f423a61fdd01df7eb0f972beb99c86a3a88740e3
sovnya <enp7s1@tutanota.com>
2024-10-24 20:30:09 +0200

    Add basic interface for Hoarder Bugs

---
+++ src/ContentLib.EnemyAPI/Model/Enemy/Vanilla/HoarderBug/IHoarderBug.cs
@@ -0,0 +1,11 @@
+﻿using ContentLib.Core.Model.Entity.Util;
+
+namespace ContentLib.EnemyAPI.Model.Enemy.Vanilla.HoarderBug;
+
+/// <summary>
+/// Interface representing the general functionality of a Hoarder Bug.
+/// </summary>
+public interface IHoarderBug : IEnemy, IKillable
+{
+    
+}
